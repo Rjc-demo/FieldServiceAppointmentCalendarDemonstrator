5a3d748146326cf1fd6063fc5154016365a724d4
rjc-demo <n/a>
2020-02-23 11:49:11 +0000

    First Compile - default everything.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FieldServiceAppointmentCalendar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
